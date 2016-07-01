c034d53d46df86a4c4a2475f7b2a4791b5aa6adb
Adriana <dri19tcc@gmail.com>
2016-06-30 21:19:15 -0700

    Added playing functionality and can check a winner.  It's the brute force way of doing it

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,45 +1,103 @@
 $(document).on('ready', function() {
   console.log('create and begin the game here!');
+
+  var ttt = new TicTacToe()
+  var is_x = true
+
   $('.up-left.boxes').mousedown(function () {
-    $('.up-left.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.up-left.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.middle-left.boxes').mousedown(function () {
-    $('.middle-left.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.middle-left.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.low-left.boxes').mousedown(function () {
-    $('.low-left.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.low-left.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.up-middle.boxes').mousedown(function () {
-    $('.up-middle.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.up-middle.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.middle-middle.boxes').mousedown(function () {
-    $('.middle-middle.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.middle-middle.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.low-middle.boxes').mousedown(function () {
-    $('.low-middle.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.low-middle.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.up-right.boxes').mousedown(function () {
-    $('.up-right.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.up-right.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.middle-right.boxes').mousedown(function () {
-    $('.middle-right.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.middle-right.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+    }
   })
+
   $('.low-right.boxes').mousedown(function () {
-    $('.low-right.boxes').addClass("active")
+    if (!$(this).hasClass("active-x") && !$(this).hasClass("active-o")) {
+      $('.low-right.boxes').addClass(is_x ? "active-x" : "active-o")
+      is_x = !is_x
+      ttt.checkWin()
+
+    }
   })
 
-  function TicTacToe() {
-  }
+  function TicTacToe() {}
 
-  TicTacToe.prototype = {
-
+  TicTacToe.prototype.checkWin = function () {
+    if ($(".up-right").hasClass("active-x") && $(".middle-right").hasClass("active-x") && $(".low-right").hasClass("active-x") || $(".up-right").hasClass("active-o") && $(".middle-right").hasClass("active-o") && $(".low-right").hasClass("active-o")) {
+      alert("Someone Won!")
+      return true
+    }
   }
 })
 
-$('note').mousedown(function(event) { // instead of this line, use the data from the html
-    var note_id = $(this).data('note') + "Audio"
-    var note_id = $(this).text() + "Audio"
-    var note = $("#" + note_id)[0]
-    note.load()
-    note.play()
-  })
+//   var box_id = $(this).data('box')
+//   console.log(box_id);
+// //   // var box =
+// //
+// //   $('#tic-tac-toe .boxes').addClass("active")
+// // })
+
+// $('note').mousedown(function(event) { // instead of this line, use the data from the html
+//     var note_id = $(this).data('note') + "Audio"
+//     var note_id = $(this).text() + "Audio"
+//     var note = $("#" + note_id)[0]
+//     note.load()
+//     note.play()
+//   })
